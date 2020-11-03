0be7dcf3b6bdff4e7c03f9689e68e52bcb3533dc
longvd89 <vulongbka@gmail.com>
2020-11-03 17:20:13 +0700

    initial commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import {SidebarContainer} from "./SidebarElement";
+
+function Sidebar() {
+    return (
+        <>
+            <SidebarContainer/>
+        </>
+    );
+}
+
+export default Sidebar;
---
+++ src/components/Sidebar/SidebarElement.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export  const SidebarContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
+    align-items: flex-start;
+    width: 250px;
+    z-index: 1001;
+    background: #556ee6;
+    bottom: 0;
+    margin-top: 0;
+    position: fixed;
+    top: 70px;
+    box-shadow: 0 0.75rem 1.5rem rgba(18,38,63,.03);
+`;
